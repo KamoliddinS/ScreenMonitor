aa86efe9d4ad37b3fde876ece242bd6e44131e49
Kamoliddin Soliev <69765410+KamoliddinS@users.noreply.github.com>
2024-05-09 14:17:42 +0500

    first commit

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
 const { app, BrowserWindow } = require('electron');
+const { autoUpdater } = require("electron-updater");
 
-function createWindow() {
-  const mainWindow = new BrowserWindow({
+function createWindow () {
+  const win = new BrowserWindow({
     width: 800,
     height: 600
   });
-  mainWindow.loadFile('index.html');
+
+  win.loadFile('index.html');
+
+  autoUpdater.checkForUpdatesAndNotify();
 }
 
 app.whenReady().then(createWindow);
-``
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
